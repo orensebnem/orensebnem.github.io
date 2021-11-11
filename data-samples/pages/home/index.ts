9f3fbc697818298a766499815eece26c75ee5448
bora <boraoren@gmail.com>
2021-11-11 20:35:34 +1000

    feat(new version): 1.6.0 updated

--- data-samples/pages/home/index.ts
+++ data-samples/pages/home/index.ts
@@ -2,19 +2,22 @@ import { HomePageData } from "../../../interfaces/HomePageData";
 
 export const homePageData: HomePageData =  {
     pageTitle: {
-        "en-US": "Sebnem Oren Showcase",
+        "en-US": "Bora Oren Showcase",
     },
     hi: {
         "en-US": "Hi,",
+        "tr-TR": "Merhaba,",
     },
     Im: {
         "en-US": "I'm",
+        "tr-TR": "Ben"
     },
     name: {
-        "en-US": "Sebnem",
+        "en-US": "Bora",
     },
     title: {
-        "en-US": "Product Enthusiast",
+        "en-US": "Software Developer",
+        "tr-TR": "Yazılım Geliştirici",
     },
-    image: "sebnem.png",
+    image: "bora.png", //should put image file in public folder
 }
