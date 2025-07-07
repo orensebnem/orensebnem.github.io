cda83e05cc2f8f3f72f330763d33819d3c5b1ac0
boraoren <boraoren@users.noreply.github.com>
2025-07-07 11:20:31 +0000

    deploy: 5d19fe1dc46e552d77e467b6d5c34ea047e752ae

---
+++ _next/static/iKRaYEjFu9GwsXsP5c5re/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fideas-to-launch","\u002Fideas-to-launch\u002F[slug]","\u002Fjournals\u002F[slug]","\u002Fportfolios","\u002Fportfolios\u002F[slug]","\u002Fside-projects"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
